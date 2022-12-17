90c14a3d66a4712cac99f1e7ac7bd886c602d9e4
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2022-12-17 20:23:51 +0700

    Added 505 Error Page

---
+++ pages/500.tsx
@@ -0,0 +1,8 @@
+export default function Custom500() {
+  return (
+    <div className="flex flex-col items-center justify-center h-[100vh] text-center">
+      <p className="text-[#208ce5] text-[3.5vh] lg:text-[4.63vh]">500 - Server Error</p>
+      <p className="text-[2vh] lg:text-[2.78vh] mb-[15px]">Please refresh this page!</p>
+    </div>
+  );
+}
