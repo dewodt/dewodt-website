c9e777a362c0a2cd3068b0bdbc7b557bd15a8c70
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2022-12-20 01:34:03 +0700

    Making contact button more efficient

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,20 +13,15 @@ export default function Home() {
         <link rel="shortcut icon" href="logo.ico" />
       </Head>
       <NavBar onPage={"Home"} />
-      <div className="flex gap-y-[3vh] py-[6vh] min-h-[91.85vh] flex-col flex-wrap items-center justify-center sm:flex-row">
+      <div className="flex min-h-[91.85vh] flex-col flex-wrap items-center justify-center gap-y-[3vh] py-[6vh] sm:flex-row">
         <Image
           src={Photo}
           alt="Dewo"
           className="w-[35vh] rounded-[50%] border-[8px] border-solid border-[#208ce5] sm:mr-[3.91vw] sm:w-[46.3vh]"
         />
-        <div className="flex flex-col">
+        <div className="block">
           <Profile />
-          <div className="flex flex-row justify-around">
-            <ContactButton items="Github" />
-            <ContactButton items="LinkedIn" />
-            <ContactButton items="Mail" />
-            <ContactButton items="Insta" />
-          </div>
+          <ContactButton />
         </div>
       </div>
     </>
