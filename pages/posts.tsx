383af0c4d9d2e6fbc9f9b43ec811719f10b555e6
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2022-12-25 03:57:28 +0700

    Added interaction for searchbar and search result

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,26 +1,69 @@
+import ArticleCard from "components/articlecard";
 import NavBar from "components/navbar";
 import PageHead from "components/pagehead";
 import { useState } from "react";
 import { request } from "../lib/datocms";
 
 export default function Posts({ data }: any) {
+  const postsData = data["allPosts"];
+  const [filteredData, setFilteredData] = useState(postsData);
+  const [countResult, setCountResult] = useState(0);
+
+  function handleInput() {
+    const inputValue = (document.getElementById("input") as HTMLInputElement).value.toLowerCase();
+    if (inputValue === "") {
+      setFilteredData(postsData);
+      setCountResult(0);
+    } else {
+      const newData = postsData.filter( (item: any) => {
+        const itemTitle = item.title.toLowerCase();
+        const itemContent = item.content.toLowerCase();
+        const itemDate = (new Date(item._firstPublishedAt)).toLocaleString("id-ID", { dateStyle: "long" }).toLowerCase();
+        return itemTitle.includes(inputValue) || itemContent.includes(inputValue) || itemDate.includes(inputValue);
+      });
+      setFilteredData(newData);
+      setCountResult(filteredData.length);
+    }
+  }
+
+  function handleReset() {
+    (document.getElementById("input") as HTMLInputElement).value="";
+    setFilteredData(postsData);
+    setCountResult(0);
+  }
 
   return (
     <>
       <PageHead />
       <NavBar onPage="Posts"/>
-      <div className="min-h-[90vh] flex justify-center">
+      <div className="flex flex-col items-center gap-y-[2vh] min-h-[90vh]">
         { /* Search Bar */ }
-        <div className="flex items-center w-[70vw] h-[5.55vh] rounded-[1.85vh] bg-white sm:w-[41.67vw] sm:h-[5.55vh]">
+        <div className="flex items-center w-[75vw] h-[5.55vh] rounded-[1.85vh] bg-white sm:w-[41.67vw] sm:h-[5.55vh]">
           <svg className="h-[3vh] mx-[3vw] sm:mx-[0.7vw] fill-[#1A1C2B]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/>
           </svg>
-          <input className="bg-white text-[2.2vh] text-[#1A1C2B] focus:outline-none w-[100%] sm:text-[2.5vh]" placeholder="Search Posts" id="input" />
-          <button className="mx-[3vw] sm:mx-[0.7vw]" onClick={() => (document.getElementById("input") as HTMLInputElement).value=""}>
+          <input className="bg-white text-[2.2vh] text-[#1A1C2B] focus:outline-none w-[100%] sm:text-[2.5vh]" placeholder="Search Posts" id="input" onChange={handleInput}/>
+          <button className="mx-[3vw] sm:mx-[0.7vw]" onClick={handleReset}>
             <svg className="h-[3vh] fill-[#1A1C2B]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
               <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
             </svg>
           </button>
+        </div>
+        { /* Number Found */ }
+        {
+          countResult > 0 && (
+            <div className="text-[#208ce5] text-[2.5vh]">
+              {countResult} search result was found
+            </div>
+          )
+        }
+        { /* Initial View or Search Result */ }
+        <div className="flex flex-col gap-y-[5vh] sm:flex-row">
+          {
+            filteredData.map( (item: any, index: number) => 
+              <ArticleCard key={index} data={item} />
+            )
+          }
         </div>
       </div>
     </> 
@@ -32,10 +75,13 @@ const POSTS_QUERY = `query Posts {
     id
     title
     content
-    keywords
+    tags
     image {
       id
       url
+      alt
+      width
+      height
     }
     updatedAt
     _firstPublishedAt
