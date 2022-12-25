74ac1d974148f52145aca2817a035aa93855676f
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2022-12-26 01:19:14 +0700

    Fixed 'lag' bug in search result, Fixed styling in search result

--- components/articlecard.tsx
+++ components/articlecard.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ArticleCard({ data }: any) {
   const publishedDate = (new Date(data._firstPublishedAt)).toLocaleString("id-ID", { dateStyle: "long" });
   return (
-    <div className="w-[75vw] h-fit p-[2.3vh] bg-[white] rounded-[2.78vh] sm:w-[41.67vh]">
-      <Image className="w-[37.03vh] mb-[1.38vh] rounded-[1.85vh]" src={data.image.url} alt={data.image.alt} width={data.image.width} height={data.image.height} />
-      <div className="flex flex-row gap-x-[4vw] mb-[0.92vh] sm:gap-x-[0.78vw]">
-        {
-          data.tags.map( (item: string) => 
-            <div className="py-[0.55vh] px-[3vw] w-fit bg-[#208ce5] text-[1.4vh] rounded-[0.925vh] sm:px-[0.7vw]" key={item}>{item}</div>
-          )
-        }
-      </div>
-      <p className="text-[#1A1C2B] text-[2.78vh] leading-tight line-clamp-1">{data.title}</p>
-      <p className="text-[#1A1C2B] text-[1.39vh] leading-tight mb-[0.92vh]">{publishedDate}</p>
-      <p className="text-[#1A1C2B] text-[1.85vh] leading-tight font-semibold line-clamp-3 text-justify">{data.content}</p>
-    </div>
+    <Link href={`posts/${data.id}`}>
+      <button className="w-[75vw] h-fit p-[2.3vh] bg-[white] rounded-[2.78vh] sm:w-[41.67vh] duration-300 hover:-translate-y-1 hover:scale-105">
+        <Image className="w-[100%] mb-[1.38vh] rounded-[1.85vh]" priority={false} src={data.image.url} alt={data.image.alt} width={data.image.width} height={data.image.height} />
+        <div className="flex flex-row flex-wrap justify-start gap-x-[4vw] mb-[0.92vh] sm:gap-x-[0.78vw]">
+          {
+            data.tags.map( (item: string) => 
+              <div className="py-[0.55vh] px-[3vw] w-fit bg-[#208ce5] text-[1.4vh] rounded-[0.925vh] sm:px-[0.7vw]" key={item}>{item}</div>
+            )
+          }
+        </div>
+        <p className="text-[#1A1C2B] text-left text-[2.78vh] leading-tight line-clamp-1">{data.title}</p>
+        <p className="text-[#1A1C2B] text-left text-[1.39vh] leading-tight mb-[0.92vh]">{publishedDate}</p>
+        <p className="text-[#1A1C2B] text-justify text-[1.85vh] leading-tight line-clamp-3">{data.content}</p>
+      </button>
+    </Link>
   );
 }
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -8,12 +8,14 @@ export default function Posts({ data }: any) {
   const postsData = data["allPosts"];
   const [filteredData, setFilteredData] = useState(postsData);
   const [countResult, setCountResult] = useState(0);
+  const [isEmpty, setIsEmpty] = useState(true);
 
   function handleInput() {
     const inputValue = (document.getElementById("input") as HTMLInputElement).value.toLowerCase();
     if (inputValue === "") {
       setFilteredData(postsData);
       setCountResult(0);
+      setIsEmpty(true);
     } else {
       const newData = postsData.filter( (item: any) => {
         const itemTitle = item.title.toLowerCase();
@@ -22,7 +24,8 @@ export default function Posts({ data }: any) {
         return itemTitle.includes(inputValue) || itemContent.includes(inputValue) || itemDate.includes(inputValue);
       });
       setFilteredData(newData);
-      setCountResult(filteredData.length);
+      setCountResult(newData.length);
+      setIsEmpty(false);
     }
   }
 
@@ -30,38 +33,41 @@ export default function Posts({ data }: any) {
     (document.getElementById("input") as HTMLInputElement).value="";
     setFilteredData(postsData);
     setCountResult(0);
+    setIsEmpty(true);
   }
 
   return (
     <>
       <PageHead />
       <NavBar onPage="Posts"/>
-      <div className="flex flex-col items-center gap-y-[2vh] min-h-[90vh]">
-        { /* Search Bar */ }
-        <div className="flex items-center w-[75vw] h-[5.55vh] rounded-[1.85vh] bg-white sm:w-[41.67vw] sm:h-[5.55vh]">
-          <svg className="h-[3vh] mx-[3vw] sm:mx-[0.7vw] fill-[#1A1C2B]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/>
-          </svg>
-          <input className="bg-white text-[2.2vh] text-[#1A1C2B] focus:outline-none w-[100%] sm:text-[2.5vh]" placeholder="Search Posts" id="input" onChange={handleInput}/>
-          <button className="mx-[3vw] sm:mx-[0.7vw]" onClick={handleReset}>
-            <svg className="h-[3vh] fill-[#1A1C2B]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-              <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
+      <div className="flex flex-col items-center min-h-[90vh] mt-[3vh] mb-[8vh] sm:mt-[6vh] sm:mb-[10vh]">
+        <div className="flex flex-col gap-y-[1.85vh] h-[13vh] items-center mb-[2vh] sm:mb-[3vh]">
+          { /* Search Bar */ }
+          <div className="flex items-center w-[75vw] h-[5.55vh] rounded-[1.85vh] bg-white sm:w-[41.67vw]">
+            <svg className="h-[3vh] mx-[3vw] sm:mx-[0.7vw] fill-[#1A1C2B]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/>
             </svg>
-          </button>
-        </div>
-        { /* Number Found */ }
-        {
-          countResult > 0 && (
-            <div className="text-[#208ce5] text-[2.5vh]">
-              {countResult} search result was found
-            </div>
-          )
-        }
-        { /* Initial View or Search Result */ }
-        <div className="flex flex-col gap-y-[5vh] sm:flex-row">
+            <input className="bg-white text-[2.2vh] text-[#1A1C2B] focus:outline-none w-[100%] sm:text-[2.5vh]" placeholder="Search Posts" id="input" onChange={handleInput}/>
+            <button className="mx-[3vw] sm:mx-[0.7vw]" onClick={handleReset}>
+              <svg className="h-[3vh] fill-[#1A1C2B]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
+              </svg>
+            </button>
+          </div>
+          { /* Number Found */ }
           {
-            filteredData.map( (item: any, index: number) => 
-              <ArticleCard key={index} data={item} />
+            (!isEmpty) && (
+              <div className="text-[#208ce5] text-[2.5vh]">
+                {countResult} search result was found
+              </div>
+            )
+          }
+        </div>
+        { /* Initial View or Search Result */ }
+        <div className="flex flex-wrap justify-center w-[80vw] gap-x-[4vw] gap-y-[6vh]">
+          {
+            filteredData.map( (item: any) => 
+              <ArticleCard key={item.id} data={item} />
             )
           }
         </div>
