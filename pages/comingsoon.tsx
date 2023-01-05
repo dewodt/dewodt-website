e0a1621d84583ce7249b6eff942b24c998a8c609
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-01-05 23:18:20 +0700

    Fixed change vw vh unit in coming soon page

--- pages/comingsoon.tsx
+++ pages/comingsoon.tsx
@@ -6,9 +6,10 @@ export default function ComingSoon() {
     <>
       <PageHead />
       <NavBar onPage={"Coming Soon"} />
-      <div className="flex min-h-[90vh] items-center justify-center text-center text-[3.5vh] sm:text-[4.63vh] font-bold">
+      <div className="flex min-h-[calc(100vh-5rem)] items-center justify-center text-center text-3xl font-bold leading-normal md:leading-normal md:text-5xl">
         <div>
-          This page{" "}<span className="text-[#208ce5]">will be</span><br />
+          This page <span className="text-[#208ce5]">will be</span>
+          <br />
           released <span className="text-[#208ce5]">soon!</span>
         </div>
       </div>
