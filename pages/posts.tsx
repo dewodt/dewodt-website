23b7868f15228fc6fdb11f31c1d49e86deff76d7
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-01-09 01:20:41 +0700

    Added scrolling animation for article card in Posts page

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -2,14 +2,20 @@ import ArticleCard from "components/articlecard";
 import NavBar from "components/navbar";
 import PageHead from "components/pagehead";
 import SearchBar from "components/searchbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { request } from "../lib/datocms";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Posts({ data }: any) {
   const postsData = data["allPosts"];
   const [filteredData, setFilteredData] = useState(postsData);
   const [countResult, setCountResult] = useState(0);
   const [isEmpty, setIsEmpty] = useState(true);
+
+  useEffect(() => {
+    AOS.init();
+  }, [])
 
   function handleChange() {
     const inputValue = (document.getElementById("input") as HTMLInputElement).value.toLowerCase();
@@ -55,7 +61,16 @@ export default function Posts({ data }: any) {
         </div>
         <div className="grid justify-center gap-x-14 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
           {filteredData.map((item: any) => (
-            <ArticleCard key={item.id} data={item} />
+            <div 
+              key={item.id}
+              data-aos="slide-up"
+              data-aos-once="true"
+              data-aos-duration="400"
+              data-aos-easing="ease-out-quad"
+              data-aos-anchor-placement="center-bottom"
+            >
+              <ArticleCard data={item} />
+            </div>
           ))}
         </div>
       </div>
