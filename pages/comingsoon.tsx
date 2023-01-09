64536c19127f98c731e9d7e67407f92bfa90b254
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-01-10 00:08:11 +0700

    Added animation to article and changed some animation to zoom in

--- pages/comingsoon.tsx
+++ pages/comingsoon.tsx
@@ -10,7 +10,7 @@ export default function ComingSoon() {
         headTag="coming soon"
       />
       <NavBar onPage={"Coming Soon"} />
-      <div className="absolute top-20 w-full h-[calc(100%-5rem)] flex items-center justify-center text-center text-3xl font-bold leading-normal 2xl:text-5xl 2xl:leading-normal animate-fadeIn animate-ease-out animate-fast border-red border-[2px] border-solid">
+      <div className="absolute top-20 w-full h-[calc(100%-5rem)] flex items-center justify-center text-center text-3xl font-bold leading-normal 2xl:text-5xl 2xl:leading-normal animate-zoomIn animate-ease-out animate-fast">
         <div>
           This page <span className="text-[#208ce5]">will be</span>
           <br />
