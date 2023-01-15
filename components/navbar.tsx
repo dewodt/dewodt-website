8e634f75f1dcf4bbd6dbcd1cd8ffd02c08093a9f
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-01-16 00:12:13 +0700

    Added animation to navbar icon in small viewport

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,22 +28,9 @@ export default function NavBar({ onPage }: { onPage: string }) {
         <Item url="/posts" page="Posts" onPage={onPage} />
         <Item url="/comingsoon" page="Coming Soon" onPage={onPage} />
       </ul>
-      {expandNav ? (
+        {/* Bar Logo */}
         <button
-          className="absolute top-[calc(2.5rem-1.125rem)] right-6 md:hidden"
-          onClick={handleClickNav}
-        >
-          <svg
-            className="h-9 fill-white"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 320 512"
-          >
-            <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
-          </svg>
-        </button>
-      ) : (
-        <button
-          className="absolute top-[calc(2.5rem-1rem)] right-6 md:hidden"
+          className={`absolute top-[calc(2.5rem-1rem)] right-6 ${everClicked && (!expandNav ? "animate-rotateIn animate-ease-out animate-faster" : "animate-rotateOut animate-ease-out animate-faster")} md:hidden`}
           onClick={handleClickNav}
         >
           <svg
@@ -54,7 +41,19 @@ export default function NavBar({ onPage }: { onPage: string }) {
             <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
           </svg>
         </button>
-      )}
+        {/* X Logo */}
+        <button
+          className={`absolute top-[calc(2.5rem-1.125rem)] right-6 ${everClicked ? (!expandNav ? "animate-rotateOut animate-ease-out animate-faster" : "animate-rotateIn animate-ease-out animate-faster") : "hidden"} md:hidden`}
+          onClick={handleClickNav}
+        >
+          <svg
+            className="h-9 fill-white"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
+          </svg>
+        </button>
     </nav>
   );
 }
