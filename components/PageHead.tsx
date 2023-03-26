187dffb0f96d86033691aaf8d2ed1bb31178023d
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-03-26 15:31:19 +0700

    fix(pagehead): Moved link preview image to cms.

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -1,37 +1,38 @@
 import Head from "next/head";
 
 interface typePageHead {
-  headTitle: string;
-  headDescription: string;
-  headTag: string;
+  pageTitle: string;
+  pageDescription: string;
+  pageTag: string[];
+  linkPreviewImage: string;
 }
 
-export default function PageHead({
-  headTitle,
-  headDescription,
-  headTag,
-}: typePageHead) {
+const PageHead = ({
+  pageTitle,
+  pageDescription,
+  pageTag,
+  linkPreviewImage,
+}: typePageHead) => {
   return (
     <Head>
-      <title>{headTitle}</title>
+      <title>{pageTitle}</title>
       <link rel="shortcut icon" href="logo.svg" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="title" content={headTitle} />
-      <meta name="description" content={headDescription} />
+      <meta name="title" content={pageTitle} />
+      <meta name="description" content={pageDescription} />
       <meta charSet="UTF-8" />
-      <meta
-        name="keywords"
-        content={`Dewantoro Triatmojo, Dewo, dewodt, ${headTag}`}
-      />
+      <meta name="keywords" content={pageTag.join(", ")} />
       <meta name="author" content="Dewantoro Triatmojo" />
 
       {/* Social Media */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://dewodt.com/" />
-      <meta property="og:title" content={headTitle} />
-      <meta property="og:description" content={headDescription} />
-      <meta property="og:image" content="https://dewodt.com/linkpreview.jpg" />
+      <meta property="og:title" content={pageTitle} />
+      <meta property="og:description" content={pageDescription} />
+      <meta property="og:image" content={linkPreviewImage} />
       <meta property="twitter:card" content="summary_large_image" />
     </Head>
   );
-}
+};
+
+export default PageHead;
